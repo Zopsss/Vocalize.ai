f0642ef68b654076e2608d2bad1b274a57f75f7d
Mauryan Kansara <mauryankansaramk@gmail.com>
2026-01-22 15:54:17 +0530

    added sidebar

---
+++ src/components/generated-avatar.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { botttsNeutral, initials } from "@dicebear/collection";
+import { createAvatar } from "@dicebear/core";
+
+import { cn } from "@/lib/utils";
+
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+interface Props {
+  seed: string;
+  className?: string;
+  variant: "initials" | "botttsNeutral";
+}
+
+const GeneratedAvatar = ({ seed, className, variant }: Props) => {
+  let avatar;
+
+  if (variant === "botttsNeutral") {
+    avatar = createAvatar(botttsNeutral, {
+      seed,
+    });
+  } else {
+    avatar = createAvatar(initials, {
+      seed,
+      fontWeight: 400,
+      fontSize: 42,
+    });
+  }
+  return (
+    <Avatar className={cn(className)}>
+      <AvatarImage src={avatar.toDataUri()} alt="Avatar" />
+      <AvatarFallback>{seed.charAt(0).toUpperCase()}</AvatarFallback>
+    </Avatar>
+  );
+};
+
+export default GeneratedAvatar;
