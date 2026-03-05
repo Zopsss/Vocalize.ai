0c1b643a9e718c42943133e28ad270b4d7423edf
Mauryan Kansara <mauryankansaramk@gmail.com>
2026-03-05 15:03:50 +0530

    Added agents table

---
+++ src/components/empty-state.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+interface Props {
+  title: string;
+  description: string;
+}
+
+export const EmptyState = ({ title, description }: Props) => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Image src={"/empty.svg"} width={240} height={240} alt="Empty" />
+      <div className="flex flex-col gap-y-4 text-center">
+        <h6 className="text-lg font-medium">{title}</h6>
+        <p className="text-sm text-muted-foreground">{description}</p>
+      </div>
+    </div>
+  );
+};
---
+++ src/modules/agents/ui/components/columns.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { GetAgent } from "../../types";
+import { ColumnDef } from "@tanstack/react-table";
+import { CornerDownRightIcon, VideoIcon } from "lucide-react";
+
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import { Badge } from "@/components/ui/badge";
+
+export const columns: ColumnDef<GetAgent>[] = [
+  {
+    accessorKey: "name",
+    header: "Agent Name",
+    cell: ({ row }) => (
+      <div className="flex flex-col gap-y-2">
+        <div className="flex items-center gap-x-3">
+          <GeneratedAvatar
+            variant="botttsNeutral"
+            seed={row.original!.name}
+            className="size-6"
+          />
+          <span className="font-semibold capitalize">{row.original!.name}</span>
+        </div>
+        <div className="px-2 flex items-center gap-x-2 [&>svg]:size-4 text-muted-foreground">
+          <CornerDownRightIcon className="size-3 text-muted-foreground/80" />
+          <span className="text-sm text-muted-foreground/80 max-w-[200px] truncate capitalize">
+            {row.original?.instructions}
+          </span>
+        </div>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "meetingCount",
+    header: "Meetings",
+    cell: ({ row }) => (
+      <Badge
+        variant={"outline"}
+        className="flex items-center gap-x-2 [&>svg:size-4] rounded-md"
+      >
+        <VideoIcon />5 Meetings
+        {/* {row.original.meetingCount}{" "}
+        {row.original.meetingCount > 1 ? "Meetings" : "Meeting"} */}
+      </Badge>
+    ),
+  },
+];
